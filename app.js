758426d4d101fe8a7d829fdf935bfb5f84f61a9f
Gabriel Vinícius <watashicx@gmail.com>
2025-02-04 15:28:22 -0300

    Adicionar amigo

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let nomeEntrada = document.getElementById("amigo");
+    if (nomeEntrada.value == ""){
+        console.log(nomeEntrada);
+        alert("Por favor, insira um nome.")
+    } else {
+        amigos.push(nomeEntrada.value);
+        console.log(amigos);
+        return nomeEntrada = "";
+    }
+}
